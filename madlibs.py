4f434279b78d366bcbd056c655f0ff5b5fc3c515
Ricky <rickywolf28@yahoo.com>
2019-08-27 16:13:30 -0700

    this commit will redo variables and add a dictionary to store all variables

--- madlibs.py
+++ madlibs.py
@@ -1,23 +1,34 @@
-myadjective= ""
-mynumber = 0
-myoccupation = ""
-myplace = ""
-mymalename = ""
-mybodypart = ""
-mynoun = ""
-mycelebrity= ""
-myverbing= ""
-myadverb = ""
-myverbs = ""
+import math
+import random
 
-adjectives = []
-number = []
-occupations = []
-places = []
-male_names = []
-body_parts = []
-nouns = []
-celebrities = []
-verbs_with_ing = []
-adverbs = []
-verbs_with_s = []
+thismadlib = {
+"number" : 0,
+"occupation1" : "",
+"occupation2" : "",
+"place1" : "",
+"male" : "",
+"place2" : "",
+"occupation3" : "",
+"bodypart1" : "",
+"adjective" : "",
+"noun" : "",
+"bodypart2" : "",
+"celebrity": "",
+"verbing": "",
+"adverb" : "",
+"verbs" : ""
+}
+
+def user_input(prompt):
+    # the input function will display a message in the terminal
+    # and wait for user input.
+    user_input = input(prompt)
+    return user_input
+
+    
+running = True
+while running:
+    selection = user_input(
+        "Press A to Add to list, R to Read from list, L to Display list, U to Update item, D to Destroy item, M to Mark Complete/Incomplete, and Q to quit: ")
+    selection_both_cases = selection.lower()
+    running = select(selection_both_cases)
